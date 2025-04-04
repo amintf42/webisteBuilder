ba9bc1090e2d744f2c16536e9039c02dc1f83709
mohammadreza salek <rezasalek75@gmail.com>
2025-04-04 20:51:06 +0330

    [mod] update route for start project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <div className="flex w-full h-full justify-center items-center">
       <Link
-        href={"/editor"}
+        href={"/editor/12"}
         className="px-5 py-3 bg-blue-400 rounded-2xl font-bold text-black"
       >
         Go to editor
